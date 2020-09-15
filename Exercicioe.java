6ad76d01b9b4b08aa39672a5c6b62da167d6c205
gabriellylimadasilva <gaaby_lima@hotmail.com>
2020-09-14 23:21:53 -0300

    Lista
    ListadeExercicios

---
+++ Exercicioe.java
@@ -0,0 +1,22 @@
+/*
+Faça um programa que recebe um número inteiro e exibe uma mensagem indicando se ele é par ou ímpar (use o operador % para obter o resto de uma divisão inteira).
+ */
+package exercicio_prof_hamilton;
+
+import javax.swing.JOptionPane;
+
+public class Exercicioe {
+    public static void main(String[] args)
+    {
+        int n;
+        n = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+        if(n % 2 == 0){
+            System.out.println("número é par: "+ n);
+        }else{
+            System.out.println("número é impar: " + n);
+            
+        }
+    }
+}
+
+
---
+++ Exercicioj.java
@@ -0,0 +1,37 @@
+/*
+Faça um programa que recebe os coeficientes a, b e C  (a ≠ 0) de uma equação de segundo grau e exibe os valores das raízes, 
+se forem reais, ou a mensagem "nao existem raizes reais", caso contrário.
+delta = b² -4ac
+raizquadrada = (-b+(√delta)) / (2*a)
+ */
+package exercicio_prof_hamilton;
+
+import javax.swing.JOptionPane;
+
+public class Exercicioj {
+   public static void main(String[] args)
+   {
+      
+        double a, b, c,raiz1, raiz, raiz2, delta;
+       
+        a = Double.parseDouble(JOptionPane.showInputDialog("Digite primeiro coeficiente a"));  
+        b = Double.parseDouble(JOptionPane.showInputDialog("Digite primeiro coeficiente b")); 
+        c = Double.parseDouble(JOptionPane.showInputDialog("Digite primeiro coeficiente c")); 
+       
+        delta = (b*b) -(4*a*c);
+        if (delta > 0){
+            raiz1 = ((-b) + Math.sqrt(delta)) / (2*a);
+            raiz2 = ((-b) - Math.sqrt(delta)) / (2*a);
+            System.out.println("Raizes Reais e Distintas \nRaiz1: " +raiz1 + "\nRaiz2: " +raiz2);
+        }
+        else if (delta == 0){ 
+            raiz1 = ((-b) + Math.sqrt(delta)) / (2*a);
+            raiz2 = ((-b) - Math.sqrt(delta)) / (2*a);
+            System.out.println("Raiz Reais e Iguais: \nRaiz1: " +raiz1 + "\nRaiz2: " +raiz2);
+        }
+        else{
+            System.out.println("Raiz complexa não podemos continuar: ");
+        }
+    } 
+}
+
